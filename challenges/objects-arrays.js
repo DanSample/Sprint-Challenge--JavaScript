f33b61307ffb200b0e3e2b74b03e201e58724d40
Dan Sample <danssample@gmail.com>
2019-04-06 09:52:34 -0700

    Built out .map() request

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -130,8 +130,8 @@ console.log(animalNames);
 The zoos need a list of all their animal's names (names only, not scientific) converted to lower case.  Create a new array named lowerCase and map over each name to convert them all to lower case.  Log the resut.
 
 */
-
 const lowerCase = [];
+zooAnimals.map(name => lowerCase.push(name.animal_name.toLowerCase())) 
 console.log(lowerCase); 
 
 /* Request 3: .filter() 
